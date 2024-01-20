5398aa4109f16f352a30538bea78d9f768be8de3
Logan Ketelaars <l_ketelaars@hotmail.com>
2024-01-20 09:49:43 -0500

    added escape to pause, modified vignette

--- lib/core.py
+++ lib/core.py
@@ -2,6 +2,7 @@ from data.scenes.start.start import Start
 from lib.camera import Camera
 from lib.cursor import Cursor
 from lib.particles import Particles
+from lib.text import Text
 from lib.player import Player
 import pygame
 
@@ -14,9 +15,18 @@ class Core:
         self.camera = Camera(self)
         particleimg = pygame.transform.scale_by(pygame.image.load("data/assets/objects/particle.png").convert_alpha(), 0.5)
         self.particles = Particles(self, image=particleimg)
+        self.paused = False
+        self.pausetext = Text("Paused", "helvetica", 36, [255, 255, 255])
 
     def loop(self, events):
         self.events = events
+        for event in self.events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.paused = not self.paused
+        if self.paused:
+            self.pausetext.loop()
+            return
         self.particles.loop()
         self.scene.loop()
         self.camera.loop()
